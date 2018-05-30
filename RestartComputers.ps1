d5fe52e41c616da2989b3866a9e5bc92db482760
mdotsherwood <34212682+mdotsherwood@users.noreply.github.com>
2018-05-30 13:03:37 -0400

    Add output.

--- RestartComputers.ps1
+++ RestartComputers.ps1
@@ -2,7 +2,7 @@ param([string] $OU = "CN=Computers,DC=testenv,DC=int")
 
 $computers = Get-ADComputer -LDAPFilter "(department=RESTART)" -SearchBase $OU
 
-Write-Host "Computers to restart: "
+Write-Host "Restart in progress.... "
 foreach($computer in $computers){
 
   Write-Host "Restarting $($computer.name)..."
@@ -10,7 +10,7 @@ foreach($computer in $computers){
   invoke-command -scriptblock {
     shutdown /m $computer.name /r /f /t 5
   } -ArgumentList $computer.name
-  
+
 }
 
 # test branching
