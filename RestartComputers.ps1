ddb7c38c8a92066049481afee597fd8ffb82fa8c
mdotsherwood <34212682+mdotsherwood@users.noreply.github.com>
2018-05-30 08:45:32 -0400

    Update RestartComputers.ps1

--- RestartComputers.ps1
+++ RestartComputers.ps1
@@ -5,6 +5,8 @@ $computers = Get-ADComputer -LDAPFilter "(department=RESTART)" -SearchBase $OU
 Write-Host "Computers to restart: "
 foreach($computer in $computers){
 
+  Write-Host "Restarting $($computer.name)..."
+
   invoke-command -scriptblock {
   		shutdown /m $computer.name /r /f /t 5
   	} -ArgumentList $computer.name
