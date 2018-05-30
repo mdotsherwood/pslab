6b7f236857a50f0bbc7af09baf2563aeae077c9e
mdotsherwood <34212682+mdotsherwood@users.noreply.github.com>
2018-05-29 20:32:30 -0400

    Update

--- RestartComputers.ps1
+++ RestartComputers.ps1
@@ -3,4 +3,4 @@ param([string] $OU = "CN=Computers,DC=testenv,DC=int")
 $computers = Get-ADComputer -LDAPFilter "(department=RESTART)" -SearchBase $OU
 
 Write-Host "Computers to restart: "
-Write-Host $computers
+Write-Host $computers.name
