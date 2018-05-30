b15ecc056bc6de77a15c8ba7977c28e79b2d63a2
mdotsherwood <34212682+mdotsherwood@users.noreply.github.com>
2018-05-30 08:02:32 -0400

    Commit RestartComputers.ps1

---
+++ RestartComputers.ps1
@@ -0,0 +1,8 @@
+param([string] $OU = "CN=Computers,DC=testenv,DC=int")
+
+$computers = Get-ADComputer -LDAPFilter "(department=RESTART)" -SearchBase $OU
+
+Write-Host "Computers to restart: "
+foreach($computer in $computers){
+  $computer.name
+}
