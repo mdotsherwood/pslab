7212a1117d0f90796a612986fc8a49f940d780ad
mdotsherwood <34212682+mdotsherwood@users.noreply.github.com>
2018-05-31 20:26:14 -0400

    Formatting.

--- RestartComputers.ps1
+++ RestartComputers.ps1
@@ -17,17 +17,17 @@ Write-Host "Date/Time: $($date)"
 foreach($computer in $computers){
 
 	Write-Host "=========="
-    Write-Host "Restarting $($computer.name)..."
+  Write-Host "Restarting $($computer.name)..."
 	try{
 		Restart-Computer -ComputerName $computer.name -ErrorAction Stop
-		Write-Host "Restart command issued successfully to $($computer.name)."	
+		Write-Host "Restart command issued successfully to $($computer.name)."
 	}
 	catch{
 		$ErrorMessage = $_.Exception.Message
 		Write-Host "Failed to restart $($computer.name)."
 		Write-Host "Error: $($ErrorMessage)"
 	}
-	
+
   # OLD SCHOOL WAY
   #invoke-command -scriptblock {
   #  shutdown /m $computer.name /r /f /t 5
