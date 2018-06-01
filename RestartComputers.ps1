001b20e5af43ce809dba2c15b21921b059d097a0
mdotsherwood <34212682+mdotsherwood@users.noreply.github.com>
2018-06-01 19:04:03 -0400

    Logging

--- RestartComputers.ps1
+++ RestartComputers.ps1
@@ -1,11 +1,12 @@
 #Define parameters and default values
 param([string] $OU = "CN=Computers,DC=testenv,DC=int", [string] $attribute="department", [string] $match="RESTART")
 
+$filename = 'C:\Reboots.log'
 #Print informational messages
-Write-Host "Starting reboot script..."
+"Starting reboot script..." | Out-File -FilePath $filename -append
 $date = Get-Date -format g
-Write-Host "Date/Time: $($date)"
-Write-Host "Retrieving list of computers..."
+"Date/Time: $($date)" | Out-File -FilePath $filename -append
+"Retrieving list of computers..." | Out-File -FilePath $filename -append
 
 #Get a list of computers based on OU membership and attribute value. Print error message and terminate if unsuccessful.
 try{
@@ -13,29 +14,29 @@ try{
 }
 catch{
 	$ErrorMessage = $_.Exception.Message
-	Write-Host "An error has occurred while retrieving a list of computers."
-	Write-Host "Error Message: $($ErrorMessage)"
+	"An error has occurred while retrieving a list of computers." | Out-File -FilePath $filename -append
+	"Error Message: $($ErrorMessage)" | Out-File -FilePath $filename -append
 	Break
 }
 
 #Iterate through computers and restart each. If restart command is unable to be issued, print error and move along.
 foreach($computer in $computers){
-	Write-Host "=========="
-	Write-Host "Restarting $($computer.name)..."
+	"==========" | Out-File -FilePath $filename -append
+	"Restarting $($computer.name)..." | Out-File -FilePath $filename -append
 	try{
 		Restart-Computer -ComputerName $computer.name -ErrorAction Stop
-		Write-Host "Restart command issued successfully to $($computer.name)."
+		"Restart command issued successfully to $($computer.name)." | Out-File -FilePath $filename -append
 	}
 	catch{
 		$ErrorMessage = $_.Exception.Message
-		Write-Host "Failed to restart $($computer.name)."
-		Write-Host "Error: $($ErrorMessage)"
+		"Failed to restart $($computer.name)." | Out-File -FilePath $filename -append
+		"Error: $($ErrorMessage)" | Out-File -FilePath $filename -append
 	}
 }
 
 #Print informational messages
 $date = Get-Date -Format g
-Write-Host "=========="
-Write-Host "Done."
-Write-Host "Date/Time: $($date)"
-Write-Host "=========="
+"==========" | Out-File -FilePath $filename -append
+"Done." | Out-File -FilePath $filename -append
+"Date/Time: $($date)" | Out-File -FilePath $filename -append
+"==========" | Out-File -FilePath $filename -append
